d9534655301892b9d6020ab4c9f2bb3c73f5fc4b
wasimodo947-pixel <wasimodo947@gmail.com>
2026-05-23 17:59:33 +0200

    Commit final: los 6 archivos completos que me pasaste. Proyecto limpio.

--- driver/src/spoofers/smbios_spoofer.cpp
+++ driver/src/spoofers/smbios_spoofer.cpp
@@ -5,7 +5,7 @@
 static PDRIVER_OBJECT g_AcpiDriver = NULL;
 static PDRIVER_DISPATCH g_OriginalSystemControl = NULL;
 
-static NTSTATUS HookedAcpiSystemControl(...) { ... }
+static NTSTATUS HookedAcpiSystemControl(PDEVICE_OBJECT DeviceObject, PIRP Irp) { ... }
 
 void InitSmbiosSpoofer() { ... }
 void CleanupSmbiosSpoofer() { ... }
