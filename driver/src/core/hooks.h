d8cce414236fb9c8af61f1620a08b33cc2d77848
wasimodo947-pixel <wasimodo947@gmail.com>
2026-05-23 20:25:12 +0200

    hooks.h actualizado con el código exacto que me pasaste.

--- driver/src/core/hooks.h
+++ driver/src/core/hooks.h
@@ -1,12 +1,5 @@
 #pragma once
 #include "common.h"
 
-typedef struct _HOOK_INFO {
-    PVOID TargetAddress;
-    PVOID HookFunction;
-    UCHAR OriginalBytes[5];
-    BOOLEAN Installed;
-} HOOK_INFO;
-
 NTSTATUS InstallHookX64(PVOID Target, PVOID HookFunction, HOOK_INFO* Hook);
 void RemoveHookX64(HOOK_INFO* Hook);
