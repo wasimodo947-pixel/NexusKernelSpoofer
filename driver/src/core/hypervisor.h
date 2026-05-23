943921ddf32badc4ca2e69b6a4da79b842eadf45
wasimodo947-pixel <wasimodo947@gmail.com>
2026-05-23 20:26:09 +0200

    hypervisor.h actualizado con el código exacto que me pasaste.

--- driver/src/core/hypervisor.h
+++ driver/src/core/hypervisor.h
@@ -14,6 +14,9 @@ extern VMX_CONTROLS g_Vmx;
 
 VOID EptHidePage(UINT64 PhysAddr, BOOLEAN Hide);
 VOID EptSetFakePage(UINT64 PhysAddr, PVOID FakePageVa);
+PUINT64 EptSplitTo4Kb(UINT64 Pml4Phys, UINT64 GuestPhysAddr);
 
 NTSTATUS InitHypervisor();
 VOID CleanupHypervisor();
+
+extern VOID VmxLaunch(UINT64 HostStackPtr, UINT64 GuestRip);
